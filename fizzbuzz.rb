c76bbef312a5d7eb616ac5806578017a02f8ddbb
Natalia Castro <nat.alvz@gmail.com>
2018-04-29 01:15:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,22 @@
 def fizzbuzz(int)
+<<<<<<< HEAD
   if (int % 3  == 0 && int % 5 == 0)
    return "FizzBuzz"
 end
+=======
+>>>>>>> 2ce263c1c576368633c2a630cd51aa8c797e6b0a
    if int % 3 == 0
    return "Fizz"
  end
 if int % 5 == 0
    return "Buzz"
  end
+<<<<<<< HEAD
 
+=======
+if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
+end
+>>>>>>> 2ce263c1c576368633c2a630cd51aa8c797e6b0a
 
  end
